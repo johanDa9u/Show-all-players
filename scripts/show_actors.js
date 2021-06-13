70767778eb0e3e3451ae6650fef526a11a7e5500
Wassim <wassimayari10@gmail.com>
2021-06-13 10:09:05 +0200

    First commit

---
+++ scripts/show_actors.js
@@ -0,0 +1,111 @@
+function getActorByName(name) {
+    let actors = game.actors._source;
+    var actor_id;
+    for (var i = 0; i < actors.length; i++) {
+        if (actors[i].name === name) {
+            actor_id = actors[i]._id;
+            break;
+        }
+    }
+    return game.actors.get(actor_id);
+}
+
+function getActorBioHTML(act) {
+    return act.sheet.getData().actor.data.details.biography.value;
+}
+
+function getActorImgHTML(act) {
+    return "<p><img src='" + act.sheet.object.data.img + "'  /></p><p>&nbsp;</p>";
+}
+
+function getModuleFolderID() {
+    var folders = game.folders._source;
+    for (var i = 0; i < folders.length; i++) {
+        if (folders[i].name === "Actors and Items") {
+            return folders[i]._id;
+        }
+    }
+    return null;
+}
+
+function moduleFolderExists() {
+    return getModuleFolderID() !== null;
+}
+
+function JournalEntryExists(name) {
+    var journalEntrys = game.journal._source;
+    for (var i = 0; i < journalEntrys.length; i++) {
+        if (journalEntrys[i].name === name) {
+            return journalEntrys[i]._id;
+        }
+    }
+    return null;
+}
+
+async function createModuleFolder() {
+    var folderData = '{"_id":null, "name":"Actors and Items", "type":"JournalEntry"}';
+    return Folder.create(JSON.parse(folderData)).then(function (s) {
+        return s._id;
+    });
+}
+
+async function createModuleFolderIfNotExist() {
+    var moduleFolderId = getModuleFolderID();
+    if (getModuleFolderID() === null) {
+        return createModuleFolder();
+    }
+    else {
+        return new Promise((res, rej) => {
+            res(moduleFolderId);
+        });
+    }
+}
+
+function getActiveActorSheet() {
+    if (!game.user.isGM) {
+        return;
+    }
+    var actors = game.actors._source;
+    var actor;
+    for (var i = 0; i < actors.length; i++) {
+        actor = game.actors.get(actors[i]._id);
+        if (actor.sheet.rendered && !actor.sheet._minimized) {
+            displayActorSheetToPlayers(actor);
+            break;
+        }
+    }
+}
+
+function displayActorSheetToPlayers(actor) {
+    if (actor === null) {
+        return;
+    }
+    var je = JournalEntryExists(actor.name);
+    if (je === null) {
+        var folder_id;
+        createModuleFolderIfNotExist().then((id) => {
+            folder_id = id;
+            var journalEntryData = '{"_id":null, "name":"' + actor.name + '", "content":"' + getActorImgHTML(actor).concat(getActorBioHTML(actor)) + '", "folder":"' + folder_id + '"}';
+
+            JournalEntry.create(JSON.parse(journalEntryData)).then(() => {
+                var j = game.journal.getName(actor.name);
+                j.sheet.render(true);
+                j.show("text", true);
+            });
+        });
+    }
+    else {
+        var j = game.journal.get(je);
+        j.sheet.render(true);
+        j.show("text", true);
+    }
+}
+
+function doc_keyDownActors(e) {
+
+    if (e.ctrlKey && e.key === 'ArrowDown') {
+        getActiveActorSheet();
+    }
+}
+
+document.addEventListener('keydown', doc_keyDown, false);
---
+++ scripts/show_items.js
@@ -0,0 +1,112 @@
+function getItemByName(name) {
+    let items = game.items._source;
+    var item_id;
+    for (var i = 0; i < items.length; i++) {
+        if (items[i].name === name) {
+            item_id = items[i]._id;
+            break;
+        }
+    }
+    return game.items.get(item_id);
+}
+
+function getItemDescriptionHTML(it) {
+    return it.data.data.description.value;
+}
+
+function getItemImgHTML(it){
+    return "<p><img src='" + it.data.img + "' /></p><p>&nbsp;</p>";
+}
+
+function getModuleFolderID() {
+    var folders = game.folders._source;
+    for (var i = 0; i < folders.length; i++) {
+        if (folders[i].name === "Actors and Items") {
+            return folders[i]._id;
+        }
+    }
+    return null;
+}
+
+function moduleFolderExists() {
+    return getModuleFolderID() !== null;
+}
+
+function JournalEntryExists(name) {
+    var journalEntrys = game.journal._source;
+    for (var i = 0; i < journalEntrys.length; i++) {
+        if (journalEntrys[i].name === name) {
+            return journalEntrys[i]._id;
+        }
+    }
+    return null;
+}
+
+async function createModuleFolder() {
+    var folderData = '{"_id":null, "name":"Actors and Items", "type":"JournalEntry"}';
+    return Folder.create(JSON.parse(folderData)).then(function (s) {
+        return s._id;
+    });
+}
+
+async function createModuleFolderIfNotExist() {
+    var moduleFolderId = getModuleFolderID();
+    if (getModuleFolderID() === null) {
+        return createModuleFolder();
+    }
+    else {
+        return new Promise((res, rej) => {
+            res(moduleFolderId);
+        });
+    }
+}
+
+function getActiveItemSheet() {
+    if (!game.user.isGM) {
+        return;
+    }
+    var items = game.items._source;
+    var item;
+    for (var i = 0; i < items.length; i++) {
+        item = game.items.get(items[i]._id);
+        if (item.sheet.rendered && !item.sheet._minimized) {
+            displayItemSheetToPlayers(item);
+            console.log(item.name);
+            break;
+        }
+    }
+}
+
+function displayItemSheetToPlayers(item) {
+    if (item === null) {
+        return;
+    }
+    var je = JournalEntryExists(item.name);
+    if (je === null) {
+        var folder_id;
+        createModuleFolderIfNotExist().then((id) => {
+            folder_id = id;
+            var journalEntryData = '{"_id":null, "name":"' + item.name + '", "content":"' + getItemImgHTML(item).concat(getItemDescriptionHTML(item)) + '", "folder":"' + folder_id + '"}';
+
+            JournalEntry.create(JSON.parse(journalEntryData)).then(() => {
+                var j = game.journal.getName(item.name);
+                j.sheet.render(true);
+                j.show("text", true);
+            });
+        });
+    }
+    else {
+        var j = game.journal.get(je);
+        j.sheet.render(true);
+        j.show("text", true);
+    }
+}
+
+function doc_keyUpItems(e) {
+
+    if (e.ctrlKey && e.key === 'ArrowUp') {
+        getActiveItemSheet();
+    }
+}
+
+document.addEventListener('keyup', doc_keyUp, false);
